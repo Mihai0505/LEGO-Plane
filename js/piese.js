7fa1fc5b1667b60f863a46367eb42eda779546bf
Mihai0505 <118223998+Mihai0505@users.noreply.github.com>
2024-10-10 22:59:15 +0300

    Add files via upload

---
+++ js/piese.js
@@ -0,0 +1,687 @@
+let piese = [
+
+
+  //ampenaje
+    {
+      "image": "images/ampenaje/vertical.png",
+      "name": "tail01",
+      "material": "plastic",
+      "cantitate": "x2",
+      "color": "white",
+      "x": "64",
+      "y": "24",
+      "z": "4.7",
+      "fisier": "pieces/tailplane/tail01.blend",
+      "parte": "ampenaje"
+    },
+    {
+      "image": "images/ampenaje/orizontal.png",
+      "name": "tail02",
+      "material": "plastic",
+      "cantitate": "x1",
+      "color": "blue",
+      "x": "39.3",
+      "y": "16",
+      "z": "105",
+      "fisier": "pieces/tailplane/tail02.blend",
+      "parte": "ampenaje"
+    },
+    {
+      "image": "images/ampenaje/suport.png",
+      "name": "tail03",
+      "material": "plastic",
+      "cantitate": "x1",
+      "color": "blue",
+      "x": "16",
+      "y": "8",
+      "z": "5",
+      "fisier": "pieces/tailplane/tail03.blend",
+      "parte": "ampenaje"
+    },
+    {
+      "image": "images/ampenaje/coada.png",
+      "name": "tail04",
+      "material": "plastic",
+      "cantitate": "x1",
+      "color": "white",
+      "x": "16",
+      "y": "16",
+      "z": "9",
+      "fisier": "pieces/tailplane/tail04.blend",
+      "parte": "ampenaje"
+    },
+
+//tren de aterizare
+
+{
+  "image": "images/tren/roata.png",
+  "name": "gear01",
+  "material": "rubber",
+  "cantitate": "x6",
+  "color": "black",
+  "x": "16",
+  "y": "4.3",
+  "z": "16",
+  "fisier": "pieces/landing/gear01.blend",
+  "parte": "trenDeAterizare"
+},
+{
+  "image": "images/tren/fata.png",
+  "name": "gear02",
+  "material": "plastic",
+  "cantitate": "x1",
+  "color": "grey",
+  "x": "21.4",
+  "y": "13.6",
+  "z": "26.8",
+  "fisier": "pieces/landing/gear02.blend",
+  "parte": "trenDeAterizare"
+},
+{
+  "image": "images/tren/spate.png",
+  "name": "gear03",
+  "material": "plastic",
+  "cantitate": "x2",
+  "color": "grey",
+  "x": "16",
+  "y": "22",
+  "z": "21",
+  "fisier": "pieces/landing/gear03.blend",
+  "parte": "trenDeAterizare"
+},
+
+//aripi
+
+{
+  "image": "images/aripi/capat.png",
+  "name": "wing01",
+  "material": "plastic",
+  "cantitate": "x2",
+  "color": "white",
+  "x": "28.3",
+  "y": "45.3",
+  "z": "4.7",
+  "fisier": "pieces/wings/wing01.blend",
+  "parte": "aripi"
+},
+{
+  "image": "images/aripi/lunga_unghi.png",
+  "name": "wing02",
+  "material": "plastic",
+  "cantitate": "x2",
+  "color": "white",
+  "x": "96",
+  "y": "24",
+  "z": "4.7",
+  "fisier": "pieces/wings/wing02.blend",
+  "parte": "aripi"
+},
+{
+  "image": "images/aripi/lunga.png",
+  "name": "wing03",
+  "material": "plastic",
+  "cantitate": "x3",
+  "color": "white",
+  "x": "80",
+  "y": "16",
+  "z": "5",
+  "fisier": "pieces/wings/wing03.blend",
+  "parte": "aripi"
+},
+{
+  "image": "images/aripi/mama.png",
+  "name": "wing04",
+  "material": "plastic",
+  "cantitate": "x4",
+  "color": "grey",
+  "x": "8",
+  "y": "4.94",
+  "z": "2.5",
+  "fisier": "pieces/wings/wing04.blend",
+  "parte": "aripi"
+},
+{
+  "image": "images/aripi/mare.png",
+  "name": "wing05",
+  "material": "plastic",
+  "cantitate": "x2",
+  "color": "white",
+  "x": "96",
+  "y": "48",
+  "z": "4.7",
+  "fisier": "pieces/wings/wing05.blend",
+  "parte": "aripi"
+},
+{
+  "image": "images/aripi/patrat.png",
+  "name": "wing06",
+  "material": "plastic",
+  "cantitate": "x2",
+  "color": "white",
+  "x": "8",
+  "y": "8",
+  "z": "4.5",
+  "fisier": "pieces/wings/wing06.blend",
+  "parte": "aripi"
+},
+{
+  "image": "images/aripi/scurta_unghi.png",
+  "name": "wing07",
+  "material": "plastic",
+  "cantitate": "x2",
+  "color": "white",
+  "x": "48",
+  "y": "24",
+  "z": "4.7",
+  "fisier": "pieces/wings/wing07.blend",
+  "parte": "aripi"
+},
+{
+  "image": "images/aripi/subtire.png",
+  "name": "wing08",
+  "material": "plastic",
+  "cantitate": "x2",
+  "color": "white",
+  "x": "48",
+  "y": "8",
+  "z": "4.5",
+  "fisier": "pieces/wings/wing08.blend",
+  "parte": "aripi"
+},
+{
+  "image": "images/aripi/tata.png",
+  "name": "wing09",
+  "material": "plastic",
+  "cantitate": "x4",
+  "color": "white",
+  "x": "16",
+  "y": "15.7",
+  "z": "4.5",
+  "fisier": "pieces/wings/wing09.blend",
+  "parte": "aripi"
+},
+
+//motor
+
+
+{
+  "image": "images/motor/aerisire.png",
+  "name": "engi01",
+  "material": "plastic",
+  "cantitate": "x9",
+  "color": "white",
+  "x": "16",
+  "y": "8",
+  "z": "3",
+  "fisier": "pieces/engine/engi01.blend",
+  "parte": "motoare"
+},
+{
+  "image": "images/motor/carcasa.png",
+  "name": "engi02",
+  "material": "plastic",
+  "cantitate": "x2",
+  "color": "grey",
+  "x": "48",
+  "y": "24",
+  "z": "27",
+  "fisier": "pieces/engine/engi02.blend",
+  "parte": "motoare"
+},
+{
+  "image": "images/motor/dreptunghi.png",
+  "name": "engi03",
+  "material": "plastic",
+  "cantitate": "x2",
+  "color": "white/blue",
+  "x": "24",
+  "y": "16",
+  "z": "5",
+  "fisier": "pieces/engine/engi03.blend",
+  "parte": "motoare"
+},
+{
+  "image": "images/motor/inversata.png",
+  "name": "engi04",
+  "material": "plastic",
+  "cantitate": "x4",
+  "color": "grey",
+  "x": "16",
+  "y": "16",
+  "z": "11.5",
+  "fisier": "pieces/engine/engi04.blend",
+  "parte": "motoare"
+},
+{
+  "image": "images/motor/la_unghi.png",
+  "name": "engi05",
+  "material": "plastic",
+  "cantitate": "x4",
+  "color": "grey",
+  "x": "16",
+  "y": "8",
+  "z": "6.5",
+  "fisier": "pieces/engine/engi05.blend",
+  "parte": "motoare"
+},
+{
+  "image": "images/motor/mica.png",
+  "name": "engi06",
+  "material": "plastic",
+  "cantitate": "x6",
+  "color": "grey",
+  "x": "16",
+  "y": "8",
+  "z": "4.5",
+  "fisier": "pieces/engine/engi06.blend",
+  "parte": "motoare"
+},
+{
+  "image": "images/motor/pale.png",
+  "name": "engi07",
+  "material": "plastic",
+  "cantitate": "x2",
+  "color": "grey",
+  "x": "19.5",
+  "y": "19.5",
+  "z": "3.5",
+  "fisier": "pieces/engine/engi07.blend",
+  "parte": "motoare"
+},
+{
+  "image": "images/motor/patrat_mare.png",
+  "name": "engi08",
+  "material": "plastic",
+  "cantitate": "x6",
+  "color": "grey",
+  "x": "16",
+  "y": "16",
+  "z": "5",
+  "fisier": "pieces/engine/engi08.blend",
+  "parte": "motoare"
+},
+{
+  "image": "images/motor/prindere.png",
+  "name": "engi09",
+  "material": "plastic",
+  "cantitate": "x2",
+  "color": "grey",
+  "x": "5.5",
+  "y": "5.5",
+  "z": "15",
+  "fisier": "pieces/engine/engi09.blend",
+  "parte": "motoare"
+},
+{
+  "image": "images/motor/prindere_fuzelaj.png",
+  "name": "engi10",
+  "material": "plastic",
+  "cantitate": "x4",
+  "color": "grey",
+  "x": "16",
+  "y": "23",
+  "z": "11",
+  "fisier": "pieces/engine/engi10.blend",
+  "parte": "motoare"
+},
+{
+  "image": "images/motor/prindere_motor.png",
+  "name": "engi11",
+  "material": "plastic",
+  "cantitate": "x2",
+  "color": "grey",
+  "x": "6.6",
+  "y": "15.4",
+  "z": "23",
+  "fisier": "pieces/engine/engi11.blend",
+  "parte": "motoare"
+},
+{
+  "image": "images/motor/prindere_pale.png",
+  "name": "engi12",
+  "material": "plastic",
+  "cantitate": "x2",
+  "color": "grey",
+  "x": "5",
+  "y": "5",
+  "z": "14",
+  "fisier": "pieces/engine/engi12.blend",
+  "parte": "motoare"
+},
+{
+  "image": "images/motor/rotunda.png",
+  "name": "engi13",
+  "material": "plastic",
+  "cantitate": "x4",
+  "color": "grey",
+  "x": "16",
+  "y": "16",
+  "z": "4.5",
+  "fisier": "pieces/engine/engi13.blend",
+  "parte": "motoare"
+},
+{
+  "image": "images/motor/turbo.png",
+  "name": "engi14",
+  "material": "plastic",
+  "cantitate": "x2",
+  "color": "grey",
+  "x": "12",
+  "y": "13.5",
+  "z": "13.5",
+  "fisier": "pieces/engine/engi14.blend",
+  "parte": "motoare"
+},
+
+//fuzelaj
+
+
+{
+  "image": "images/fuzelaj/bec.png",
+  "name": "fuse01",
+  "material": "plastic",
+  "cantitate": "x4",
+  "color": "red/green",
+  "x": "8",
+  "y": "8",
+  "z": "4.5",
+  "fisier": "pieces/fuselage/fuse01.blend",
+  "parte": "fuzelaj"
+},
+{
+  "image": "images/fuzelaj/cap.png",
+  "name": "fuse02",
+  "material": "plastic",
+  "cantitate": "x1",
+  "color": "white",
+  "x": "81",
+  "y": "48",
+  "z": "39.5",
+  "fisier": "pieces/fuselage/fuse02.blend",
+  "parte": "fuzelaj"
+},
+{
+  "image": "images/fuzelaj/cap_jos.png",
+  "name": "fuse03",
+  "material": "plastic",
+  "cantitate": "x1",
+  "color": "blue",
+  "x": "81",
+  "y": "48",
+  "z": "11.7",
+  "fisier": "pieces/fuselage/fuse03.blend",
+  "parte": "fuzelaj"
+},
+{
+  "image": "images/fuzelaj/coada.png",
+  "name": "fuse04",
+  "material": "plastic",
+  "cantitate": "x1",
+  "color": "blue",
+  "x": "48",
+  "y": "81",
+  "z": "39.6",
+  "fisier": "pieces/fuselage/fuse04.blend",
+  "parte": "fuzelaj"
+},
+{
+  "image": "images/fuzelaj/coada_sus.png",
+  "name": "fuse05",
+  "material": "plastic",
+  "cantitate": "x1",
+  "color": "blue",
+  "x": "48",
+  "y": "81",
+  "z": "11.7",
+  "fisier": "pieces/fuselage/fuse05.blend",
+  "parte": "fuzelaj"
+},
+{
+  "image": "images/fuzelaj/geam.png",
+  "name": "fuse06",
+  "material": "plastic",
+  "cantitate": "x20",
+  "color": "dark green",
+  "x": "12.5",
+  "y": "1.2",
+  "z": "15.5",
+  "fisier": "pieces/fuselage/fuse06.blend",
+  "parte": "fuzelaj"
+},
+{
+  "image": "images/fuzelaj/geam_cap.png",
+  "name": "fuse07",
+  "material": "plastic",
+  "cantitate": "x1",
+  "color": "dark green",
+  "x": "27.1",
+  "y": "45.2",
+  "z": "11",
+  "fisier": "pieces/fuselage/fuse07.blend",
+  "parte": "fuzelaj"
+},
+{
+  "image": "images/fuzelaj/jos.png",
+  "name": "fuse08",
+  "material": "plastic",
+  "cantitate": "x6",
+  "color": "blue",
+  "x": "48",
+  "y": "32",
+  "z": "11.7",
+  "fisier": "pieces/fuselage/fuse08.blend",
+  "parte": "fuzelaj"
+},
+{
+  "image": "images/fuzelaj/jumatate.png",
+  "name": "fuse09",
+  "material": "plastic",
+  "cantitate": "x6",
+  "color": "white",
+  "x": "16",
+  "y": "8",
+  "z": "11.2",
+  "fisier": "pieces/fuselage/fuse09.blend",
+  "parte": "fuzelaj"
+},
+{
+  "image": "images/fuzelaj/lata_6.png",
+  "name": "fuse10",
+  "material": "plastic",
+  "cantitate": "x23",
+  "color": "white/blue",
+  "x": "32",
+  "y": "16",
+  "z": "5",
+  "fisier": "pieces/fuselage/fuse10.blend",
+  "parte": "fuzelaj"
+},
+{
+  "image": "images/fuzelaj/lunga1.png",
+  "name": "fuse11",
+  "material": "plastic",
+  "cantitate": "x2",
+  "color": "grey",
+  "x": "112",
+  "y": "8",
+  "z": "11.2",
+  "fisier": "pieces/fuselage/fuse11.blend",
+  "parte": "fuzelaj"
+},
+{
+  "image": "images/fuzelaj/lunga2.png",
+  "name": "fuse12",
+  "material": "plastic",
+  "cantitate": "x2",
+  "color": "grey",
+  "x": "128",
+  "y": "8",
+  "z": "11.2",
+  "fisier": "pieces/fuselage/fuse12.blend",
+  "parte": "fuzelaj"
+},
+{
+  "image": "images/fuzelaj/panou.png",
+  "name": "fuse13",
+  "material": "plastic",
+  "cantitate": "x6",
+  "color": "white",
+  "x": "16",
+  "y": "8",
+  "z": "20.5",
+  "fisier": "pieces/fuselage/fuse13.blend",
+  "parte": "fuzelaj"
+},
+{
+  "image": "images/fuzelaj/panou_geam.png",
+  "name": "fuse14",
+  "material": "plastic",
+  "cantitate": "x10",
+  "color": "white",
+  "x": "32",
+  "y": "8",
+  "z": "20.5",
+  "fisier": "pieces/fuselage/fuse14.blend",
+  "parte": "fuzelaj"
+},
+{
+  "image": "images/fuzelaj/plat.png",
+  "name": "fuse15",
+  "material": "plastic",
+  "cantitate": "x11",
+  "color": "white",
+  "x": "16",
+  "y": "16",
+  "z": "3",
+  "fisier": "pieces/fuselage/fuse15.blend",
+  "parte": "fuzelaj"
+},
+{
+  "image": "images/fuzelaj/plat_lung.png",
+  "name": "fuse16",
+  "material": "plastic",
+  "cantitate": "x3",
+  "color": "white",
+  "x": "32",
+  "y": "16",
+  "z": "3",
+  "fisier": "pieces/fuselage/fuse16.blend",
+  "parte": "fuzelaj"
+},
+{
+  "image": "images/fuzelaj/subtire_4.png",
+  "name": "fuse17",
+  "material": "plastic",
+  "cantitate": "x24",
+  "color": "blue",
+  "x": "32",
+  "y": "8",
+  "z": "4.5",
+  "fisier": "pieces/fuselage/fuse17.blend",
+  "parte": "fuzelaj"
+},
+{
+  "image": "images/fuzelaj/sus.png",
+  "name": "fuse18",
+  "material": "plastic",
+  "cantitate": "x6",
+  "color": "white",
+  "x": "48",
+  "y": "32",
+  "z": "11.5",
+  "fisier": "pieces/fuselage/fuse18.blend",
+  "parte": "fuzelaj"
+}
+
+//48 piese total listate
+];
+
+
+function schimbare(mat){
+  console.log(cautare);
+  let da = 0;
+  let list = document.querySelector("#item-list-all");
+  list.innerHTML = '';
+    
+    piese.forEach(piesa =>{
+          if("all" == mat || piesa.parte == mat || piesa.name == mat){
+            da = 1;
+            list.innerHTML += `
+    
+            <div class="wrapper">
+            <div class="container2">
+              <div class="top">
+                <img src="${piesa.image}">
+              </div>
+              <div class="bottom">
+                <div class="left">
+                  <div class="details">
+                    <h1>${piesa.name}</h1>
+                    <strong>${piesa.cantitate}</strong>
+                  </div>
+                  <div class="buy"><a href="${piesa.fisier}" download><i class="fa fa-download"></i></a></div>
+                </div>
+              </div>
+            </div>
+            <div class="inside">
+              <div class="icon"><i class="fa fa-info-circle"></i></div>
+              <div class="contents">
+                <table>
+                  <tr>
+                    <th>X</th>
+                    <th>Material</th>
+                  </tr>
+                  <tr>
+                    <td>${piesa.x} mm</td>
+                    <td>${piesa.material}</td>
+                  </tr>
+                  <tr>
+                    <th>Y</th>
+                    <th>Color</th>
+                  </tr>
+                  <tr>
+                    <td>${piesa.y} mm</td>
+                    <td>${piesa.color}</td>
+                  </tr>
+                  <tr>
+                    <th>Z</th>
+                    <th>File</th>
+                  </tr>
+                  <tr>
+                    <td>${piesa.z} mm</td>
+                    <td>.blend</td>
+                  </tr>
+                </table>
+              </div>
+            </div>
+            </div>
+            
+            `;
+
+
+          }
+    });
+
+   if(da == 0){list.innerHTML = `<h2 class="nuExista">The piece with code "${mat}" does not exist!</h2>`;}
+
+}
+
+document.getElementById("defaultOpen").click();
+
+
+/** menu piese **/
+$(function() {
+  $('body').addClass('js');
+
+  var $hamburger = $('.hamburger'),
+      $nav = $('#site-nav'),
+      $masthead = $('#masthead');
+
+  $hamburger.click(function() {
+    $(this).toggleClass('is-active');
+    $nav.toggleClass('is-active');
+    $masthead.toggleClass('is-active');
+    return false; 
+  })
+});
+ 
